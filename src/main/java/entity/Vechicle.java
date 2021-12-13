8b20e87570f724ad726ba76aa2ddf2740dbf8b48
Mato <matosevicpatrik@gmail.com>
2021-12-13 19:49:30 +0100

    1.Fixed Incoming and outgoing roads. 2.Changed the value of isRiding to false after the rout is finished. 3.Calculating one step forward for intersection arrival. 4.Made intersections bigger so the simulation can run without slowdowns.

--- src/main/java/entity/Vechicle.java
+++ src/main/java/entity/Vechicle.java
@@ -12,6 +12,7 @@ public class Vechicle extends Actor{
     private Queue<Road> route;
     private boolean isRiding = true;
     private boolean isFinished = false;
+    private boolean next = false;
 
     public Vechicle(float speed, List<Road> route, Simulation simulation) {
         super(0,0, simulation);
@@ -32,55 +33,41 @@ public class Vechicle extends Actor{
     public void tick(double elapsedTime) {
         if (!isRiding) return;
         if (isFinished) return;
-
-        //move my x, and y according to speed, and elapsedTime
-
-        //kako točno računat premik avta v updated funkciji z elapsedTime?
-
-
-        // dobimo en step tako da delimo razdaljo med zacetno x coordinato od koncne in delimo s totalTicks
-        //float oneStepX = (float) ((sim.getXY(route.peek().getStartId()).getX()-sim.getXY(route.peek().getEndId()).getX())/totalTicks);
-        //float oneStepY = (float) ((sim.getXY(route.peek().getStartId()).getY()-sim.getXY(route.peek().getEndId()).getY())/totalTicks);
-
         //dont be afraid of references
         Road currentRoad = route.peek();
 
+        float totalTicks = currentRoad.getLength() / this.speed; // dobimo vse tick-e tako da delimo dolžino v metrih z hitrostjo v m/s
+
+        float oneStepX = ((currentRoad.getStart().x - currentRoad.getEnd().x) / totalTicks)*(-1);
+        float oneStepY = ((currentRoad.getStart().y - currentRoad.getEnd().y) / totalTicks)*(-1);
+
+        float oneRealStepX = ((float) (oneStepX * elapsedTime));
+        float oneRealStepY = ((float) (oneStepY * elapsedTime));
+
+
         //--------------CIRCLE AROUND INTERSECTIONS CALCULATION----------------
         float prviParameter = (currentRoad.getEnd().x - this.x)*(currentRoad.getEnd().x - this.x);
+        float prviParameterNext = (currentRoad.getEnd().x - this.x+oneRealStepX)*(currentRoad.getEnd().x - this.x+oneRealStepX);
         float drugiParameter = (currentRoad.getEnd().y - this.y)*(currentRoad.getEnd().y - this.y);
+        float drugiParameterNext = (currentRoad.getEnd().y - this.y+oneRealStepY)*(currentRoad.getEnd().y - this.y+oneRealStepY);
 
         float sestevek = prviParameter + drugiParameter;
+        float sestevekNext = prviParameterNext + drugiParameterNext;
 
 
-        float d = 5*5 - sestevek;
+        float d = 10*10 - sestevek;
+        float dNext = 10*10 - sestevekNext;
         //---------------------------------------------------------------------
 
-        //System.out.println("D je: " + d);
-
         //If radius squared of the intersection is >= 0 then we are at the intersection...
-        if (d > 0) {
-            //System.out.println("Done");
+        if (d > 0 || next) {
             isRiding = false;
             if (sim.getIntersection(currentRoad.getEnd().getId()).canIGo()) nextRoad();
-
-            //route.remove();
         } else {
-            //System.out.println("Not done!!!");
-            float totalTicks = currentRoad.getLength() / this.speed; // dobimo vse tick-e tako da delimo dolžino v metrih z hitrostjo v m/s
-            //System.out.println("Total ticks: " + totalTicks);
-            float oneStepX = ((currentRoad.getStart().x - currentRoad.getEnd().x) / totalTicks)*(-1);
-            float oneStepY = ((currentRoad.getStart().y - currentRoad.getEnd().y) / totalTicks)*(-1);
-            //System.out.println("One step of X: " + oneStepX);
-            //System.out.println("One step of Y: " + oneStepY);
-
-            //System.out.println("Acctual step of X: " + (float) (oneStepX * elapsedTime));
-            //System.out.println("Acctual step of Y: " + (float) (oneStepY * elapsedTime));
-
+            if (dNext > 0) next=true;
             // dobimo trenutne koordinate tako, da množimo steps z elapsed time z one Step
-            this.x = this.x + ((float) (oneStepX * elapsedTime));
-            this.y = this.y + ((float) (oneStepY * elapsedTime));
-            //System.out.println(this.x);
-            //System.out.println(this.y);
+            this.x = this.x + oneRealStepX;
+            this.y = this.y + oneRealStepY;
 
         }
     }
@@ -95,13 +82,14 @@ public class Vechicle extends Actor{
         //System.out.println(route.peek());
         if (route.size() > 1) {
             route.remove();
+            next=false;
             this.x = route.peek().getStart().x;
             this.y = route.peek().getStart().y;
         }
         isRiding = true;
         if (route.isEmpty()) {
             isFinished = true;
-            isRiding = true;
+            isRiding = false;
         }
 
     }
